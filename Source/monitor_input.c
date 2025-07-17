d9c0a71c93f1bf5d25d50a9521f1195f22f10ace
engrron30 <ronaldtorresrosete30.com>
2025-07-17 17:40:28 +0800

    Working code

--- Source/monitor_input.c
+++ Source/monitor_input.c
@@ -10,28 +10,35 @@
 #define DIR_DATA                "Data/"
 
 static void process_command(const char *cmd);
-static char getch(void);
+static char get_char_without_newline(void);
 
-void monitor_input(void) 
+void monitor_input(void)
 {
     char input[CMDLINE_INPUT_LEN], input_ch;
 
     while (1) {
         printf("%s ", CMDLINE_NAME);
+        fflush(stdout);
 
-        input_ch = getch();
-        if (CMDLINE_QUERY_CMD_CHAR == input_ch) {
+        // Monitor '?' character
+        input_ch = get_char_without_newline();
+        if (input_ch == CMDLINE_QUERY_CMD_CHAR) {
             system("cat Data/?");
-        } else {
-            if (fgets(input, sizeof(input), stdin) == NULL)
-                break;
-
-            input[strcspn(input, "\n")] = 0;
-            printf("### DEBUG input = %s ###\n\n");
+            continue;
         }
 
+        // Command by User
+        ungetc(input_ch, stdin);
+        printf("%c", input_ch);
+        fflush(stdout);
+        if (fgets(input, sizeof(input), stdin) == NULL)
+            break;
+
+        input[strcspn(input, "\n")] = 0;
+        process_command(input);  // Call your command processor
     }
 }
+
 
 static void process_command(const char *cmd) {
     if (strcmp(cmd, "hello") == 0) {
@@ -44,16 +51,21 @@ static void process_command(const char *cmd) {
     }
 }
 
-static char getch(void)
+/* Waits for user to input a character and saves it to ch
+ * without waiting for the user to hit newline
+ * */
+static char get_char_without_newline(void)
 {
     struct termios oldt, newt;
     char ch;
-    tcgetattr(STDIN_FILENO, &oldt);           // get current terminal attributes
+
+    tcgetattr(STDIN_FILENO, &oldt);
     newt = oldt;
-    newt.c_lflag &= ~(ICANON | ECHO);         // disable canonical mode and echo
-    tcsetattr(STDIN_FILENO, TCSANOW, &newt);  // set new attributes
-    ch = getchar();                           // read character
-    tcsetattr(STDIN_FILENO, TCSANOW, &oldt);  // restore old attributes
+    newt.c_lflag &= ~(ICANON | ECHO);
+    tcsetattr(STDIN_FILENO, TCSANOW, &newt);
+    ch = getchar();
+    tcsetattr(STDIN_FILENO, TCSANOW, &oldt);
+
     return ch;
 }
 
