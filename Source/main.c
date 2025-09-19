3873d2f6038a03569dcfe45c4384b176200bec94
engrron30 <ronaldtorresrosete30@gmail.com>
2025-09-19 09:52:40 +0800

    Add EXEC_ENDLESSLY macro to indicate while-loop forever running

--- Source/main.c
+++ Source/main.c
@@ -3,6 +3,8 @@
 #include "monitor_input.h"
 #include "process_commands.h"
 
+#define EXEC_ENDLESSLY         true
+
 static void welcome_notes(void);
 
 int main()
@@ -12,7 +14,7 @@ int main()
 
     welcome_notes();
 
-    while (true) {
+    while (EXEC_ENDLESSLY) {
         monitor_input(user_cmd, &user_cmd_len);
         process_command(user_cmd);
     }
